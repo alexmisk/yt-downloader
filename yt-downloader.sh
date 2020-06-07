e3fc7ae6f4f45764c405fdee96d0f37256ff1114
alexmisk <alexmisk@ya.ru>
2020-06-07 20:29:26 +0300

    refactor: better file handling after upload

--- yt-downloader.sh
+++ yt-downloader.sh
@@ -5,8 +5,6 @@ youtube-dl \
   --extract-audio --audio-format mp3          \
   --output '%(title)s [%(uploader)s].%(ext)s' \
   --download-archive '/out/downloaded.txt'    \
+  --exec 'touch -am {} && mv {} /out'         \
   --add-metadata                              \
   --ignore-errors
-
-touch -am *.mp3
-mv *.mp3 /out
