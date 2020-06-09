6637dfc0eb36ae15ba6d92f3bf7589327e5d89c9
alexmisk <alexmisk@ya.ru>
2020-06-09 16:34:31 +0300

    feat: add fetch interval setting (closes #1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 while :
 do
+
   flock -n /tmp/yt-downloader.lock sh yt-downloader.sh
-  sleep 30
+  echo "Sleep..."
+
+  if [ ! -z "$FETCH_INTERVAL" ]
+  then
+    sleep $FETCH_INTERVAL
+  else
+    sleep 1d
+  fi
+
 done
