f0b14f0d90ab77f919362068403b6028776c41e2
alexmisk <alexmisk@ya.ru>
2020-07-30 10:49:40 +0300

    feat: add PLAYLIST env check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 while :
 do
+  if [ -z ${PLAYLIST+x} ]; then echo "Please set PLAYLIST environmental variable" && exit 1; fi
   youtube-dl -U
   flock -n /tmp/yt-downloader.lock sh yt-downloader.sh
   echo "Sleep for $FETCH_INTERVAL ..."
