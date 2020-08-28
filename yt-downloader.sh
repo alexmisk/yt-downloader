4fd807e792b2ab185262cf702e1d9704fd1193a1
alexmisk <alexmisk@ya.ru>
2020-08-28 12:43:09 +0300

    feat: set lower conversion audio quality

--- yt-downloader.sh
+++ yt-downloader.sh
@@ -5,6 +5,7 @@ youtube-dl $PLAYLIST \
   --output '%(title)s [%(uploader)s].%(ext)s' \
   --download-archive '/out/downloaded.txt' \
   --extract-audio --audio-format mp3 \
+  --audio-quality 9 \
   --add-metadata \
   --exec 'touch -am {} && mv {} /out' \
   --ignore-errors
